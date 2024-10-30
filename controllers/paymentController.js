f096bb68cda6c15953fccd8a73055c89403542c9
Luxho31 <137393667+Luxho31@users.noreply.github.com>
2024-10-30 13:41:52 -0500

    Limpiando el codigo

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -5,19 +5,14 @@ import Orden from '../models/Orden.js';
 import Repuesto from '../models/Repuesto.js';
 
 // const client = new MercadoPagoConfig({ accessToken: 'TEST-605985810472705-061802-bc26711c25e75c5f11986ed30bbef3e8-1762277778' }); // Token principal Wallet connect
-const client = new MercadoPagoConfig({ accessToken: 'APP_USR-911176942514250-062113-773e06516eea3d6146d37d26de4cc3f5-1864627025' }); // Token de prueba vendedor Wallet connect
-// const client = new MercadoPagoConfig({ accessToken: 'TEST-2661763250208715-062313-71f3790248b40e8fd04540a94b053e40-1762277778' }); // Token principal Checkout Pro Prueba
+// const client = new MercadoPagoConfig({ accessToken: 'APP_USR-911176942514250-062113-773e06516eea3d6146d37d26de4cc3f5-1864627025' }); // Token de prueba vendedor Wallet connect
+const client = new MercadoPagoConfig({ accessToken: 'TEST-2661763250208715-062313-71f3790248b40e8fd04540a94b053e40-1762277778' }); // Token principal Checkout Pro Prueba
 // const client = new MercadoPagoConfig({ accessToken: 'TEST-8021851533821614-032518-5e2291ab742081851d0e8355030bbab8-485417535' }); // Token Diego
 
-
-// Primero debo crear la orden
-// Recorrer los items de la orden
-// Luego debo crear la preferencia de pago
 const createOrder = async (req, res) => {
 
     const { productos, comprador } = req.body;
 
-    // Validar que productos y comprador estén definidos
     if (!productos || !Array.isArray(productos) || productos.length === 0) {
         return res.status(400).send('Productos no válidos');
     }
@@ -26,37 +21,20 @@ const createOrder = async (req, res) => {
         return res.status(400).send('Comprador no válido');
     }
 
-    // Guardar la orden en la base de datos
     const newOrder = new Orden({
         productos: productos.map(producto => ({
             title: producto.title,
             quantity: producto.quantity,
-            // description: producto.description,
             unit_price: producto.unit_price
         })),
         comprador: {
             name: comprador.name,
             surname: comprador.surname,
             email: comprador.email,
-            // phone: {
-            //     area_code: comprador.phone.area_code,
-            //     number: comprador.phone.number
-            // },
-            // identification: {
-            //     type: comprador.identification.type,
-            //     number: comprador.identification.number
-            // },
-            // address: {
-            //     street_name: 'Insurgentes Sur',
-            //     street_number: 1602,
-            //     zip_code: '03940'
-            // }
         },
     });
 
-    // Guardar la orden en la base de datos
     await newOrder.save();
-
 
     try {
         const preference = new Preference(client);
@@ -66,44 +44,22 @@ const createOrder = async (req, res) => {
                 items: productos.map(producto => ({
                     title: producto.title,
                     quantity: producto.quantity,
-                    // description: producto.description,
                     unit_price: producto.unit_price
                 })),
-                // external_reference: '200', // numero de orden.getId()
-                external_reference: '200', // numero de orden.getId()
+                external_reference: '200',
                 payer: {
                     name: comprador.name,
                     surname: comprador.surname,
                     email: comprador.email,
-                    // phone: {
-                    //     area_code: comprador.phone.area_code,
-                    //     number: comprador.phone.number
-                    // },
-                    // identification: {
-                    //     type: comprador.identification.type,
-                    //     number: comprador.identification.number
-                    // },
-                    // address: {
-                    //     street_name: 'Insurgentes Sur',
-                    //     street_number: 1602,
-                    //     zip_code: '03940'
-                    // }
                 },
                 back_urls: {
                     success: 'http://localhost:5173/',
                     failure: 'http://localhost:5173/',
                     pending: 'http://localhost:3000/api/payment/pending'
                 },
-                // auto_return: 'approved', // Hace que el usuario sea redirigido automáticamente a la URL de retorno
                 notification_url: 'https://webhook.site/d9b459b6-bea3-4839-b0d8-7c26d1457744/webhook',
             }
         })
-
-        // Guardar la orden en la base de datos y tambien la preferencia de pago y luego guardar el pyment_id en la orden
-        // Si un mismo usuario crea una nueva orden deberia cambiarse la que se creo anteriormente a fallida
-
-        // console.log(response);
-        // res.send(response.id);
 
         newOrder.preference_id = response.id;
         await newOrder.save();
@@ -117,7 +73,6 @@ const createOrder = async (req, res) => {
 }
 
 const success = (req, res) => {
-    // res.send('Payment successful');
     res.json(req.query);
 }
 
@@ -134,15 +89,12 @@ const receiveWebhook = async (req, res) => {
 
     try {
         if (payment.type === 'payment') {
-            // Aquí puedes obtener más detalles del pago si es necesario
             const data = await client.payment.findById(payment['data.id']);
             console.log(data);
         }
-        // res.sendStatus(200);
     } catch (error) {
         console.log(error);
         return res.status(500).json({ error: error.message });
-        // return res.status(500).send('Error procesando el webhook');
     }
 }
 
